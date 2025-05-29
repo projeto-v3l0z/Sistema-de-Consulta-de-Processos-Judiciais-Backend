daeacd2966790c1361e6ae6d738dde5abd0a3a33
Suzane Rodrigues <suzanerodrigues589@gmail.com>
2025-05-29 20:03:29 -0300

    criação de algumas permissões

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Colocar essa classe dentro do modelo de usuário para que as permissões sejam criadas
+class Meta:
+    permissions = [
+        ("pode_cadastrar_processo", "Pode cadastrar processo"), 
+        ("pode_editar_processo", "Pode editar processo"),
+        ("pode_excluir_processo", "Pode excluir processo"),
+        ("pode_consultar_processo", "Pode consultar processo"),
+    ]
