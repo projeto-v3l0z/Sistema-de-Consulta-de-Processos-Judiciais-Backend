5fef8bfb7b85eba45e9e7c465280d08cc53c3f1c
Suzane Rodrigues <suzanerodrigues589@gmail.com>
2025-05-30 05:03:34 -0300

    nova versão: criação de grupos e permissões

--- usuario/apps.py
+++ usuario/apps.py
@@ -1,6 +1,42 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from .models import User
 
 class UsuarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'usuario'
+
+    def ready(self):
+        # Obter ContentType para o modelo associado às permissões
+        content_type = ContentType.objects.get(app_label='usuario', model='User')
+
+        # Criar permissões
+        criar_permissões = [
+            {'codename': 'cadastrar_processos', 'name': 'Pode cadastrar processos'},
+            {'codename': 'editar_processos', 'name': 'Pode editar processos'},
+            {'codename': 'visualizar_processos', 'name': 'Pode visualizar processos'},
+            {'codename': 'excluir_processos', 'name': 'Pode excluir processos'},
+        ]
+        # Criando as permissões se não existirem
+        permissões_criadas = []
+        for perm in criar_permissões:
+            permission, _ = Permission.objects.get_or_create(
+                codename=perm['codename'],
+                name=perm['name'],
+                content_type=content_type
+            )
+            permissões_criadas.append(permission)
+
+        # Criando o grupo Operador e adicionando todas as permissões
+        operator_group, _ = Group.objects.get_or_create(name='Operador')
+        for permission in permissões_criadas:
+            operator_group.permissions.add(permission)
+        
+        # Criando o grupo Usuário Comum e adicionando apenas a permissão de visualizar processos
+        user_group, _ = Group.objects.get_or_create(name='Usuário Comum')
+        for permission in permissões_criadas:
+            if permission.codename == 'visualizar_processos':
+                user_group.permissions.add(permission)
+
+    
--- usuario/models.py
+++ usuario/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
-# Colocar essa classe dentro do modelo de usuário para que as permissões sejam criadas
-class Meta:
-    permissions = [
-        ("pode_cadastrar_processo", "Pode cadastrar processo"), 
-        ("pode_editar_processo", "Pode editar processo"),
-        ("pode_excluir_processo", "Pode excluir processo"),
-        ("pode_consultar_processo", "Pode consultar processo"),
-    ]
+# Create your models here.
